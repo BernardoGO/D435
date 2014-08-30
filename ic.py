048584a5b400bec1e3f6f13aefcc570a576da165
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2014-08-30 09:46:48 -0300

    scatter

---
+++ ic.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import matplotlib.animation as animation
+import csv
+from Tkinter import *
+import tkMessageBox
+from functools import partial
+
+
+bolas = []
+E1 = None
+texts = [];
+
+
