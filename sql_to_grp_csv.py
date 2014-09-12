36a8af97dc5e13ee545f16ca75cfb080f2100279
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2014-09-12 09:54:33 -0300

    sql to grp csv

--- sql_to_grp_csv.py
+++ sql_to_grp_csv.py
@@ -14,6 +14,47 @@ rep = open("report.txt", "w")
 rep.write("Relatorio\n")
 
 
+with open('residencial.csv', 'wb') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                            )
+        foundgrupo = False
+        soma = 0
+        dia = ""
+        consumidor = ""
+        nhorarios = 0
+        dmed = 0
+        dmax = 0
+        hdmax = ""
+        ndias = 0
+        totalcli = 0;
+        adia = None
+        bdate = None
+        dramo = ""
+
+        for row in myreader:
+        
+            
+
+            cliente, date, ramo, tipo, medidor, cod, grandeza, valor,  = row
+
+            
+
+            adia = date.split()[0]
+            #print hdmax + "   -   " + str(adia) + "   -   " + str(bdate)
+            
+
+
+
+            if cliente.isdigit() == False:
+                continue
+
+            
+                
+
+            if (cliente != consumidor) or (dia != adia):
+                
+                #31/05
+
 
             
             
