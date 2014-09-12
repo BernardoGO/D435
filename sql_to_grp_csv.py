66a9dd01a89a44a4dca903656893895f9b224379
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2014-09-12 09:54:08 -0300

    sql to grp csv

---
+++ sql_to_grp_csv.py
@@ -0,0 +1,20 @@
+import csv
+
+_MAX_CONSUMIDORES_ = 0; #20
+_GRUPO_ATV_ = "Resid" #"Beleza"
+
+
+csv_in = open('ultimoSQL.csv', 'rb')
+myreader = csv.reader(csv_in)
+
+consumidores = 0
+
+rep = open("report.txt", "w")
+
+rep.write("Relatorio\n")
+
+
+
+            
+            
+            
