caaa64b1a500b99fd108c41ff669f97e76852622
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2014-09-18 09:57:58 -0300

    sql_to_csv

--- sql_to_grp_csv.py
+++ sql_to_grp_csv.py
@@ -12,11 +12,12 @@ consumidores = 0
 rep = open("report.txt", "w")
 
 rep.write("Relatorio\n")
+comp = False
+lines = 0
 
-
-with open('residencial.csv', 'wb') as csvfile:
+with open('residencia.csv', 'wb') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',',
-                            )
+        )
         foundgrupo = False
         soma = 0
         dia = ""
@@ -54,48 +55,55 @@ with open('residencial.csv', 'wb') as csvfile:
             if (cliente != consumidor) or (dia != adia):
                 
                 #31/05
-                if consumidores > 0:
-                    dmed = round(soma / nhorarios,4)
-                    if dmed > dmax:
-                        print "ERROR"
-                        break
-                    print str(tipo) + " [] " + str(ramo) + " [] " + bdate.split()[0] + " [] " + str(dmed) + " [] " + str(dmax)
-                    spamwriter.writerow([consumidor, bdate.split()[0], dmed, dmax, hdmax,dramo])
-                    totalcli += soma
-                    rep.write("Dmed dia " + bdate.split()[0] + ":" + str(dmed) + "\n")
-                    rep.write("N de horarios dia " + bdate.split()[0] + " :" + str(nhorarios) + "\n")
-                if _GRUPO_ATV_ is not None:
-                        if _GRUPO_ATV_ in ramo:
-                                foundgrupo = True
-                        #elif _GRUPO_ATV_ in tipo:
-                        #        foundgrupo = True
-                        else:
-                                if foundgrupo == True:
-                                        break
-                                continue
-                
-                if (cliente != consumidor):
-                        if consumidores > 0:
-                                rep.write("N. de dias: " + str(ndias) + "\n")
-                                rep.write("Consumo Total: " + str(totalcli) + "\n")
-                        
-                        
-                        consumidores += 1
-                        if _MAX_CONSUMIDORES_ != 0 and consumidores >= _MAX_CONSUMIDORES_: break
-                        rep.write("\nConsumidor: " + cliente + "\n")
-                        ndias = 0
-                        totalcli = 0
+				if consumidores > 0:
+					dmed = round(soma / nhorarios,4)
+					if dmed > dmax:
+						print "ERROR"
+						break
+					print str(tipo) + " [] " + str(ramo) + " [] " + bdate.split()[0] + " [] " + str(dmed) + " [] " + str(dmax)
+					spamwriter.writerow([consumidor, bdate.split()[0], dmed, dmax, hdmax,dramo])
+					lines = lines + 1
+					totalcli += soma
+					rep.write("Dmed dia " + bdate.split()[0] + ":" + str(dmed) + "\n")
+					rep.write("N de horarios dia " + bdate.split()[0] + " :" + str(nhorarios) + "\n")
+				if _GRUPO_ATV_ is not None:
+					if _GRUPO_ATV_ in ramo:
+						foundgrupo = True
 
+					else:
+						if foundgrupo == True:
+							break
+						continue
                 
-                ndias += 1
-                soma = 0
-                dia = adia
-                consumidor = cliente
-                nhorarios = 0
-                dmax = 0
-                hdmax = None
-                dramo = ramo
-
+				if (cliente != consumidor) and ndias != 39 and  consumidores > 0:
+					while ndias != 39:
+						comp = True
+						spamwriter.writerow([consumidor, bdate.split()[0], dmed, dmax, hdmax,dramo])
+						ndias += 1
+						lines = lines + 1
+					
+				if (cliente != consumidor):
+					if consumidores > 0:
+						rep.write("N. de dias: " + str(ndias) + " Status: " + str(comp) + "\n")
+						rep.write("Consumo Total: " + str(totalcli) + "\n")
+                                
+                        
+					consumidores += 1
+					if _MAX_CONSUMIDORES_ != 0 and consumidores >= _MAX_CONSUMIDORES_: break
+					rep.write("\nConsumidor: " + cliente + "\n")
+					ndias = 0
+					totalcli = 0
+					comp = False
+                
+				ndias += 1
+				soma = 0
+				dia = adia
+				consumidor = cliente
+				nhorarios = 0
+				dmax = 0
+				hdmax = None
+				dramo = ramo
+				
             bdate = date
             
             
@@ -111,6 +119,7 @@ with open('residencial.csv', 'wb') as csvfile:
 
 rep.write("\n\nTOTAIS: ")         
 rep.write("\nN. de consumidores: " + str(consumidores))
+rep.write("\nN. de linhas: " + str(lines))
 
 rep.close()
 
